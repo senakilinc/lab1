dbffdd0b8f9c1ccfea03a0389b2779f0e6755f9c
senakilinc <sena.kilinc.001@student.uni.lu>
2021-02-02 23:52:17 +0100

    Completed exercise 1: MessageToTheWorld.

---
+++ MessageToTheWorld.java
@@ -0,0 +1,8 @@
+//package lab1;
+
+public class MessageToTheWorld {
+  public static void main(String[] args) {
+    System.out.println("My name is Sena, and I’m gonna be a coding legend.");
+  }
+}
+
